82d6bd458394b94ece647b88274aeadd8084d914
javintx <javintx@gmail.com>
2017-11-24 06:20:21 +0100

    Kata1 commit

---
+++ src/org/katas/kata1/StringCalculator.java
@@ -0,0 +1,67 @@
+package org.katas.kata1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    private static final int REGEX_GROUP_NUMBER_OF_DELIMITERS = 1;
+    private static final int REGEX_GROUP_NUMBER_OF_NUMBER_STRINGS = 2;
+    private static final String PREDEFINED_DELIMITERS_REGEX = ",|\n";
+    private static final String USER_DEFINED_DELIMITERS_REGEX = "^//(.)\n(.*)";
+    private static final String NEGATIVE_NUMBERS_NOT_ALLOWED_MESSAGE = "negatives not allowed";
+    private static final String SPACE = " ";
+
+    public int run(String numbers) {
+        Matcher m = Pattern.compile(USER_DEFINED_DELIMITERS_REGEX).matcher(numbers);
+        String delimiters = getDelimiters(m);
+        String numberString = getNumberStrings(numbers, m.reset());
+        return getSum(getAllNumbers(numberString, delimiters));
+    }
+
+    private String getNumberStrings(String numbers, Matcher m) {
+        if (m.find()) {
+            return m.group(REGEX_GROUP_NUMBER_OF_NUMBER_STRINGS);
+        } else {
+            return numbers;
+        }
+    }
+
+    private String getDelimiters(Matcher m) {
+        if (m.find()) {
+            return Pattern.quote(m.group(REGEX_GROUP_NUMBER_OF_DELIMITERS));
+        } else {
+            return PREDEFINED_DELIMITERS_REGEX;
+        }
+    }
+
+    private String[] getAllNumbers(String numbers, String delimiters) {
+        if (numbers.isEmpty()) {
+            return new String[0];
+        } else {
+            return numbers.split(delimiters);
+        }
+    }
+
+    private int getSum(String numbers[]) {
+        int sum = 0;
+        StringBuffer error = new StringBuffer();
+        for (String number : numbers) {
+            sum += toIntAndAddNegativeToError(number, error);
+        }
+        throwNegativeNumbersException(error);
+        return sum;
+    }
+
+    private void throwNegativeNumbersException(StringBuffer error) {
+        if (error.length() > 0) throw new RuntimeException(NEGATIVE_NUMBERS_NOT_ALLOWED_MESSAGE + error);
+    }
+
+    private int toIntAndAddNegativeToError(String number, StringBuffer error) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+            error.append(SPACE).append(num);
+        }
+        return num;
+    }
+}
---
+++ test/org/katas/kata1/StringCalculatorTest.java
@@ -0,0 +1,69 @@
+package org.katas.kata1;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void getStringCalculator() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void emptyStringShouldReturn0() {
+        assertEquals(0, stringCalculator.run(""));
+    }
+
+    @Test
+    public void oneNumberShouldReturnThisNumber() {
+        assertEquals(1, stringCalculator.run("1"));
+    }
+
+    @Test
+    public void twoNumbersShouldReturnSum() {
+        assertEquals(3, stringCalculator.run("1,2"));
+    }
+
+    @Test
+    public void multipleNumbersShouldReturnSum() {
+        assertEquals(6, stringCalculator.run("1,2,3"));
+    }
+
+    @Test
+    public void newDelimiterShouldBeHandled() {
+        assertEquals(6, stringCalculator.run("1\n2,3"));
+    }
+
+    @Test
+    public void userDelimiterShouldBeHandled() {
+        assertEquals(3, stringCalculator.run("//;\n1;2"));
+    }
+
+    @Test
+    public void userAndPredefinedDelimiterShouldBeHandled() {
+        assertEquals(3, stringCalculator.run("//?\n1?2"));
+    }
+
+    @Test
+    public void negativeNumberShouldBeException() {
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("negatives not allowed -1");
+        stringCalculator.run("-1");
+    }
+
+    @Test
+    public void multipleNegativeNumbersShouldBeException() {
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("negatives not allowed -1 -3");
+        stringCalculator.run("-1,2,-3");
+    }
+}
